1d85d400049baad768474a9af45dd49d85f15a86
abhishek98185 <abhishek.rameshrpvvsector11@gmail.com>
2025-05-19 22:15:02 +0530

    chnges

--- ntarip/home/urls.py
+++ ntarip/home/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index,jeeadvance
+
 
 urlpatterns = [
-    path('', index , name='home')
+    path('', index , name='home'),
+    path('jeeadvance',jeeadvance,name="jeeadvance")
 ]
+
--- ntarip/home/views.py
+++ ntarip/home/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def jeeadvance(request):
+    return render(request,'jeeadvance.html')
+

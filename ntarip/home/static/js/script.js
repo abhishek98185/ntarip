29a20800a43e353f2cfd8b09932b6328d8c562a1
LalmaniBytes <lalmani.mishra1508@gmail.com>
2025-05-20 14:17:03 +0530

    Responsiveness added

--- ntarip/home/static/js/script.js
+++ ntarip/home/static/js/script.js
@@ -3,3 +3,20 @@ function downloadFile(filename) {
   alert(`Downloading ${filename}`);
   // In real site: window.location.href = 'your-file-link-here';
 }
+document.addEventListener("DOMContentLoaded", function () {
+        document
+          .getElementById("hamburger")
+          .addEventListener("click", function () {
+            const nav = document.getElementById("navLinks");
+            nav.classList.toggle("active");
+          });
+
+        window.downloadFile = function (fileName) {
+          const link = document.createElement("a");
+          link.href = `/static/${fileName}`;
+          link.download = fileName;
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+        };
+      });

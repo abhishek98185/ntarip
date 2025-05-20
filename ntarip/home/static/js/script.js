60cc3ac37cbf0e8dda2b36404b21fe26faabb765
LalmaniBytes <lalmani.mishra1508@gmail.com>
2025-05-21 00:12:10 +0530

    String Fetaure

--- ntarip/home/static/js/script.js
+++ ntarip/home/static/js/script.js
@@ -34,3 +34,53 @@ tl.from(".navbar ul li", {
   opacity: 0,
   stagger: 0.2
 })
+var path = `M 50 0 Q 50 250 50 500`
+var finalpath = `M 50 0 Q 50 250 50 500`
+var pathContainer = document.querySelector("#path")
+pathContainer.addEventListener("mousemove", function (dets) {
+  const bounds = pathContainer.getBoundingClientRect();
+  const x = dets.clientX - bounds.left;
+  const y = dets.clientY - bounds.top;
+  path = `M 50 0 Q ${x} ${y} 50 500`
+  gsap.to("#path svg path", {
+    attr: {
+      d: path
+    },
+    duration: 0.3,
+    ease: "power3.out"
+  })
+
+})
+pathContainer.addEventListener("mouseleave", function () {
+  gsap.to("svg path", {
+    attr: {
+      d: finalpath
+    },
+    duration: 1.5,
+    ease: "elastic.out(1, 0.2)"
+  })
+})
+var path2Container = document.querySelector("#path2")
+path2Container.addEventListener("mousemove", function (dets) {
+  const bounds = path2Container.getBoundingClientRect();
+  const x = dets.clientX - bounds.left;
+  const y = dets.clientY - bounds.top;
+  path = `M 50 0 Q ${x} ${y} 50 500`
+  gsap.to("#path2 svg path", {
+    attr: {
+      d: path
+    },
+    duration: 0.3,
+    ease: "power3.out"
+  })
+
+})
+path2Container.addEventListener("mouseleave", function () {
+  gsap.to("svg path", {
+    attr: {
+      d: finalpath
+    },
+    duration: 1.5,
+    ease: "elastic.out(1, 0.2)"
+  })
+})

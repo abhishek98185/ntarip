e8226edf1d4c3038645706869d98e4b09179f6f3
TechRuler <ask995844@gmail.com>
2025-05-26 12:40:25 +0530

    exam app created

--- ntarip/ntarip/urls.py
+++ ntarip/ntarip/urls.py
@@ -20,7 +20,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('account', include('accounts.urls'))
+    path('account', include('accounts.urls')),
+    path('exam', include('exam.urls'))
 ]
 
 #username = Anmol_Kumar 

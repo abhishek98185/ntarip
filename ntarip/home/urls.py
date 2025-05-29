012f7180da37da4a7a6222768d76a7752d84ff48
LalmaniBytes <lalmani.mishra1508@gmail.com>
2025-05-29 19:44:55 +0530

    Linking to do list

--- ntarip/home/urls.py
+++ ntarip/home/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index,jeeadvance,jee,neet,formulasheet,about
+from .views import index,jeeadvance,jee,neet,formulasheet,about,todolist
 
 
 urlpatterns = [
@@ -9,6 +9,7 @@ urlpatterns = [
     path('jee',jee,name="jee"),
     path('neet',neet,name="neet"),
     path('formulasheet',formulasheet,name="formulasheet"),
-    path('about',about,name="about")
+    path('about',about,name="about"),
+    path('todolist',todolist,name="todolist")
 ]
 
